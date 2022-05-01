eb4f458ea0890b89355cc0936a491dfa84fbd725
liluyang1999 <liluyang1999@qq.com>
2022-05-01 18:52:06 +0100

    Apply EA/MOEA to optimize the complex network in certain goals'

--- elite_preservation.m
+++ elite_preservation.m
@@ -10,17 +10,17 @@ function newPopulation = elite_preservation(comPop, popSize, allFronts, crowDist
     for rank = 1 : frontSize
         curFrontIndexes = allFronts(rank).front;
         temp = count + size(curFrontIndexes, 2);
+        [~, sortIndexes] = sort(crowDists(curFrontIndexes), "descend"); 
         if temp <= popSize
             % If the remaining size of new population is large enough, 
             % incorporate all the individuals of the current front into it
-            newPopulation(count+1 : temp) = comPop(curFrontIndexes);
+            newPopulation(count+1 : temp) = comPop(curFrontIndexes(sortIndexes));
             count = count + size(curFrontIndexes, 2);
         elseif (popSize - count) > 0
             % If the remaining size of new population can't incorporate all 
             % the individuals of current front, it will be filled according
             % to the crowding distances and then return
             remSize = popSize - count;
-            [~, sortIndexes] = sort(crowDists(curFrontIndexes), "descend"); 
             newPopulation(count+1:popSize) = comPop(curFrontIndexes(sortIndexes(1:remSize)));
             return;
         else 
--- init_population.m
+++ init_population.m
@@ -1,14 +1,18 @@
-%% Randomly generate network graphs according to size of population
+%% Randomly generate network graphs according to the size of population
 function population = init_population(N, popSize)
     % The vertices number of graph is N
     population = cell(1, popSize);
     maxLinkNum = N * (N - 1) / 2;
     for index = 1 : popSize
+        % Randomly decide the initial link number (maximum: N*(N-1)/2)
+        % N links to ensure connectivity + Random number of remaining links 
         randNum = randi(maxLinkNum - N);
         linkNum = N + randNum;
         s = zeros(1, linkNum);
         t = zeros(1, linkNum);
         while true
+            % For each vertice, randomly generate a link to another vertice 
+            % to ensure the connectivity of graph
             s(1, 1:N) = randperm(N);
             for tIndex = 1 : N
                 while true
@@ -20,6 +24,8 @@ function population = init_population(N, popSize)
                 end
             end
             
+            % Randomly generate the remaining links, then make it a simple
+            % graph and check its connectivity
             s(1, N+1:linkNum) = randi(N, 1, randNum);
             t(1, N+1:linkNum) = randi(N, 1, randNum);
             graph_G = graph(s, t);
